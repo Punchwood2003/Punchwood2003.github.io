a910f975843a19a36efa6aa9280b126bd9295702
Matthew Sheldon <punchwood2003@gmail.com>
2022-12-24 19:20:16 -0600

    Minor bug fix

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+/*
+ * Retrieve the HTML for the menu items and the HTML for the navigation bar
+ */
 const menuIcon = document.querySelector('.menu-icon');
 const navBar = document.querySelector('nav');
 
-menuIcon.addEventListener('mouseover', () => {
-    menuIcon.classList.toggle('hover');
-});
-
+/*
+ * Whenever the menu icon is clicked, toggle between showing the 
+ * navigation links and not, as well as toggle between the hamburger
+ * icon and the times symbol.
+ */
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('change');
     navBar.classList.toggle('nav-hidden');
